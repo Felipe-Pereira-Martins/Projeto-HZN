8ac4f040e37d29e0873df08eead0f72b81f0060f
Felipe Pereira Martins <110059790+Felipe-Pereira-Martins@users.noreply.github.com>
2022-08-04 09:45:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const botaoMenu = document.querySelector('.cabecalho__menu')
+const menuLateral = document.querySelector('.menu-lateral')
+
+botaoMenu.addEventListener('click', () => {
+    menuLateral.classList.toggle('menu-lateral--Ativo')
+})
+
+
+
